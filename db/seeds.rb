eae2ed4fafe56175e6e3c5ae441529f69d2e70d4
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-12 15:26:58 +0100

    created seed fetching movie from api of le wagon

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "json"
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+
+ hash = user['results'].first(10)
+
+puts 'deleting movie'
+Movie.destroy_all
+puts 'database clean'
+
+puts 'creating movie'
+
+hash.each do |movie|
+
+  movie = Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"],
+    )
+
+  puts "id: #{movie.id} movie: #{movie.title}"
+
+end
+
+puts 'finished to create movie'
