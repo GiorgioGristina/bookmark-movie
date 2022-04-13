d3003e887a17906e129e9aaa557e5f7e4bc2d5ce
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-13 12:01:06 +0100

    added to bookmark controller destroy action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,10 +17,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:list_id])
+  
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
